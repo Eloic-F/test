7f3883a453a494d16333d4e16ffa26fa34e1661e
OKJ <effstarbury@gmail.com>
2024-09-28 12:59:21 +0200

    P1S4 - array

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // P1S3
+console.log('===== P1S3 =====');
 let ticket = {
   nomFilm: 'Akira',
   prix: 35,
@@ -10,3 +11,16 @@ let nom = 'Marc';
 let texteAffichage = 'Bonjour ' + nom + ', votre film ' + ticket.nomFilm + ' est en salle ' + ticket.numeroSalle + '.';
 
 console.log(texteAffichage);
+
+// P1S4
+console.log('===== P1S4 =====');
+// Creation tableau
+let playlist = ['Ask Me Know', 'op18 Moderato', 'Violets'];
+
+// Ajouter 2 nouveaux morceaux
+playlist.push('Doudou', '297');
+
+// Retirer le dernier
+playlist.pop();
+
+console.log('Contenu de la playlist :\n', playlist);
