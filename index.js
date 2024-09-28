7c747d6305f1323c2884d3c29c520e3ad87f3456
OKJ <effstarbury@gmail.com>
2024-09-28 11:52:09 +0200

    P1S3 - Manipulation objet

---
+++ index.js
@@ -0,0 +1,12 @@
+// P1S3
+let ticket = {
+  nomFilm: 'Akira',
+  prix: 35,
+  numeroSalle: 11
+}
+
+let nom = 'Marc';
+
+let texteAffichage = 'Bonjour ' + nom + ', votre film ' + ticket.nomFilm + ' est en salle ' + ticket.numeroSalle + '.';
+
+console.log(texteAffichage);
